b15d19d1a209c4639e1354de53083d745ea9b976
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-10 02:07:05 +0500

    lib modding

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-mod macros;
-mod syntax;
-
+extern crate osmon;
 extern crate structopt;
 
+use structopt::StructOpt;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "osmon", about = "Osmon's Compiler")]
+pub struct Options {
+    #[structopt(parse(from_os_str))]
+    pub file: PathBuf,
+
+}
+
 fn main() {
-    println!("{}", "Hello World")
+    println!("{}", "Hello World".to_owned())
 }
