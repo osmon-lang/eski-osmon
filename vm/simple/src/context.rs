c63d4f0d0925ff13237f2a7724dfa98db3a24738
UwUssimo Robinson <uwussimo@icloud.com>
2022-03-17 15:58:19 +0500

    simple removed due to its inactivity

--- vm/simple/src/context.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-use osmon_vm::function::Function;
-use osmon_vm::opcodes::Instruction;
-use osmon_vm::value::Value;
-
-#[derive(Debug,Clone)]
-pub struct Context {
-    pub builder: (),
-}
--- vm/simple/src/lib.rs
+++
@@ -1,6 +0,0 @@
-extern crate osmon_vm;
-
-pub use osmon_vm::*;
-
-pub mod context;
-pub mod builder;
