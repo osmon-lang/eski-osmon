1530f73d59631724eff7ad3ba821b10d131dfadf
UwUssimo Robinson <uwussimo@icloud.com>
2022-04-01 21:46:56 +0500

    Update release.ps1

--- release.ps1
+++ release.ps1
@@ -2,5 +2,5 @@
  # create a powershell script that will create a new tag and push it to the remote repo
  #>
 
-git tag -a v$1 -m "version $1"
-git push origin v$1
+git tag -a v$args[0] -m "version $1"
+git push origin v$args[0]
