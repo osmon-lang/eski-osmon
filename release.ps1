490be9169e2c91b1855dc8100043920d3053cc78
UwUssimo Robinson <uwussimo@icloud.com>
2022-04-01 23:14:19 +0500

    automated releases

--- release.ps1
+++
@@ -1,7 +0,0 @@
-<#
- # create a powershell script that will create a new tag and push it to the remote repo
- #>
-
-$version = $args[0]
-git tag -a "v$version" -m "version $version"
-git push origin "v$version"
--- release.sh
+++
@@ -1,4 +0,0 @@
-# write a script that will create git tag version and push it to github
-
-git tag -a v$1 -m "version $1"
-git push origin v$1
