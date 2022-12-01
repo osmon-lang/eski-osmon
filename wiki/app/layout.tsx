319d37d0c96901c090ff03734c581c366b83222d
UwUssimo Robinson <uwussimo@icloud.com>
2022-12-02 00:58:12 +0500

    [FIX] (wiki) changed layouts of pages

--- wiki/app/layout.tsx
+++ wiki/app/layout.tsx
@@ -1,7 +1,7 @@
 import '#/styles/globals.css';
 import { AddressBar } from '#/ui/AddressBar';
 import { GlobalNav } from '#/ui/GlobalNav';
-import { VercelLogo } from '#/ui/VercelLogo';
+import { UwULogo } from '#/ui/UwULogo';
 
 export default function RootLayout({
   children,
@@ -42,27 +42,27 @@ function Byline() {
   return (
     <div className="flex items-center justify-between space-x-4 p-3.5 lg:px-5 lg:py-3">
       <div className="flex items-center space-x-1.5">
-        <div className="text-sm text-gray-600">By</div>
-        <a href="https://vercel.com" title="Vercel">
+        <a href="https://uwussi.moe" title="UwUssimo">
           <div className="w-16 text-gray-300 hover:text-gray-50">
-            <VercelLogo />
+            <UwULogo />
           </div>
         </a>
+        <div className="text-sm text-gray-600">tomonidan yuritiladi</div>
       </div>
 
       <div className="text-sm text-gray-600">
         <a
           className="underline decoration-dotted underline-offset-4 hover:text-gray-400"
-          href="https://github.com/vercel/app-playground"
+          href="https://github.com/uwussimo/osmon"
         >
-          View code
+          Ko&lsquo;zdan kechiring
         </a>
-        {' or '}
+        {' yoki '}
         <a
           className="underline decoration-dotted underline-offset-4 hover:text-gray-400"
-          href="https://vercel.com/templates/next.js/app-directory"
+          href="https://github.com/uwussimo/osmon/tree/main/wiki"
         >
-          deploy your own
+          o&lsquo;zgartirish kiriting
         </a>
       </div>
     </div>
